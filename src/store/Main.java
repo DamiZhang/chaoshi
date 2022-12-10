fa77b354c2315f36451402e53a3f2d0ff330e9c3
DamiZhang <zihaz16@uci.edu>
2022-12-09 17:16:22 -0800

    Second Commit

--- src/store/Main.java
+++ src/store/Main.java
@@ -1,5 +1,11 @@
 package store;
-
+/*
+Final project
+[Name:Zihao Zhang]
+[Student ID: 002656320]
+[Section:8]
+[https://github.com/DamiZhang/chaoshi]
+ */
 public class Main { //set up the class Main to run the Frame
     public static void main(String[] args) {
         new Frame();
