494da93dbc66a05a89abda64df12c55998a370de
DamiZhang <62860166+DamiZhang@users.noreply.github.com>
2022-12-10 22:51:23 -0800

    Update Main.java

--- src/store/Main.java
+++ src/store/Main.java
@@ -5,6 +5,7 @@ Final project
 [Student ID: 002656320]
 [Section:8]
 [https://github.com/DamiZhang/chaoshi]
+The Default Username and Password all is 111
  */
 public class Main { //set up the class Main to run the Frame
     public static void main(String[] args) {
